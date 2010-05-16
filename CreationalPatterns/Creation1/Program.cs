51d710b5d409052173e6d5fc12ab5ead093b47e9
Chris Patterson <chris@phatboyg.com>
2010-05-15 22:42:13 -0500

    don't delete it this time moron

---
+++ CreationalPatterns/Creation1/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Creation1
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using DemoLibrary.Model;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var order = new Order();
+
+
+            Console.WriteLine("The order created is an instance of type: {0}", order.GetType());
+
+
+            ShowMethodInfo(() => Order.CreateOrder());
+            ShowMethodInfo(() => new Order());
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+        private static void ShowMethodInfo(Expression<Action> method)
+        {
+            var me = method.Body as MethodCallExpression;
+            if (me != null)
+            {
+                ShowMethodInfo(method.Body, me.Method);
+                return;
+            }
+
+            var ne = method.Body as NewExpression;
+            if (ne != null)
+            {
+                ShowMethodInfo(method.Body, ne.Constructor);
+            }
+        }
+
+        private static void ShowMethodInfo(Expression expression, MethodBase methodInfo)
+        {
+            Console.WriteLine("----");
+            Console.WriteLine("{0}", expression);
+            Console.WriteLine();
+            Console.WriteLine("Method name: {0}", methodInfo.Name);
+            if(methodInfo is MethodInfo)
+            {
+                Console.WriteLine("Return Type: {0}", ((MethodInfo)methodInfo).ReturnType);
+            }
+
+            foreach (var parameter in methodInfo.GetParameters())
+            {
+                Console.WriteLine("Parameter {0} Type: {1}", parameter.Position, parameter.ParameterType);
+                
+            }
+        }
+    }
+}
---
+++ CreationalPatterns/DemoLibrary/Model/Order.cs
@@ -0,0 +1,15 @@
+namespace DemoLibrary.Model
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+
+        public static Order CreateOrder()
+        {
+            return new Order();
+        }
+    }
+}
