fb10686169462368b41f5273229a1b2c9981911a
Chris Patterson <chris@phatboyg.com>
2010-05-21 16:54:00 -0500

    Added builder pattern

---
+++ Creation/CreationConsole/Builder/UserBuilder.cs
@@ -0,0 +1,72 @@
+namespace CreationConsole.Builder
+{
+	using System;
+
+	public abstract class UserBuilder
+	{
+		private UserLevel _level;
+		private string _username;
+
+		public virtual UserBuilder Username(string name)
+		{
+			_username = name;
+			return this;
+		}
+
+		public virtual UserBuilder Level(UserLevel level)
+		{
+			_level = level;
+			return this;
+		}
+
+		public virtual User Create()
+		{
+			return new User(_username, _level);
+		}
+	}
+
+	public class AdminUserBuilder :
+		UserBuilder
+	{
+		public override UserBuilder Level(UserLevel level)
+		{
+			if (level != UserLevel.Normal)
+				throw new InvalidOperationException("Not authorized to create administrator");
+
+			return base.Level(level);
+		}
+	}
+
+	public enum UserLevel
+	{
+		Normal,
+		Admin,
+		SuperAdmin
+	}
+
+	public class User
+	{
+		public User(string username, UserLevel level)
+		{
+			Username = username;
+			Level = level;
+		}
+
+		public string Username { get; private set; }
+		public UserLevel Level { get; private set; }
+	}
+
+
+	public class Test
+	{
+		public void Should_only_be_allowed_to_create_normal_users()
+		{
+			User user = new AdminUserBuilder()
+				.Username("regular.joe")
+				.Level(UserLevel.Normal)
+				.Create();
+
+			Console.WriteLine("User " + user.Username + " created as " + user.Level);
+		}
+	}
+}
